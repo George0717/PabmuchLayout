2b22b4f13b550aa716253683061514244018bcb1
anhaki <m.anugrahhakiki@gmail.com>
2022-11-07 11:20:02 +0700

    third commit

--- nextLayout/app/src/main/java/george/if3b/pert/nextlayout/MainActivity.java
+++ nextLayout/app/src/main/java/george/if3b/pert/nextlayout/MainActivity.java
@@ -1,14 +1,72 @@
 package george.if3b.pert.nextlayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+    private FrameLayout flContainer;
+    private BottomNavigationView bnvMusikKita;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        bnvMusikKita.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.menu_musik:
+                        bukaFragment(new MusikFragment());
+                        getSupportActionBar().setTitle("Musik");
+                        return true;
+
+                    case R.id.menu_album:
+                        bukaFragment(new AlbumFragment());
+                        getSupportActionBar().setTitle("Album");
+                        return true;
+
+                    case R.id.menu_artis:
+                        bukaFragment(new ArtisFragment());
+                        getSupportActionBar().setTitle("Artis");
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+
+    private void initView() {
+
+
+        bnvMusikKita = findViewById(R.id.bnv_musik_kita);
+        flContainer = findViewById(R.id.fl_container);
+        bukaFragment(new MusikFragment());
+        getSupportActionBar().setTitle("Musik");
+        bukaFragment(new AlbumFragment());
+        getSupportActionBar().setTitle("Album");
+        bukaFragment(new ArtisFragment());
+        getSupportActionBar().setTitle("Artis");
+    }
+
+
+    private void bukaFragment(Fragment frm){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.fl_container, frm);
+        ft.commit();
     }
 }
